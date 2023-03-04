763219204e43bf184592a58f869062e842c0eb2f
MilanRyan <100083961+MilanRyan@users.noreply.github.com>
2023-03-04 22:12:02 +0800

    2 files modified

--- src/app/httpclient/httpclient.component.ts
+++ src/app/httpclient/httpclient.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+
 import { HttpclientService } from '../../httpclient.service';
 
 @Component({
   selector: 'app-httpclient',
+
   templateUrl: './httpclient.component.html',
+  
   styleUrls: ['./httpclient.component.css'],
 })
 export class HttpclientComponent implements OnInit {
