3ac334cb1644728e10734378aaad774f93738242
MilanRyan <100083961+MilanRyan@users.noreply.github.com>
2023-03-15 21:59:50 +0800

    2 files modified

--- src/httpclient.service.ts
+++ src/httpclient.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +7,6 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 export class HttpclientService {
   constructor(private http: HttpClient) {}
   getUserfromServer() {
-    let params = new HttpParams().set('limit', 6);
     return this.http.get('https://jsonplaceholder.typicode.com/users');
   }
 }
